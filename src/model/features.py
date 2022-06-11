7fe1e25fcb4f8e9e8b5b9360078084466e1f0c4b
emdemor <emdemor415@gmail.com>
2022-06-10 21:56:36 -0300

    imp: add neighborhood data treatment

--- src/model/features.py
+++ src/model/features.py
@@ -1,7 +1,11 @@
+import unidecode
 from typing import Any
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
+from src.config import get_config
+import unidecode
+from src.base.commons import to_snake_case
 
 TARGET_COLUMN = "price"
 
@@ -37,12 +41,18 @@ TYPE_MAPPING = {
     "FARM": "COUNTRY",
 }
 
+LONG_INTERVAL = [-46.7, -46.4]
+
+LAT_INTERVAL = [-21.9, -21.76]
+
 
 def build_features(data: pd.DataFrame) -> tuple:
 
     data = clear_dataset(data)
 
     data = dataset_treatment(data)
+
+    data = construct_features(data)
 
     X = data.drop(columns=TARGET_COLUMN, errors="ignore")
 
@@ -73,28 +83,11 @@ def clear_dataset(data: pd.DataFrame) -> pd.DataFrame:
 
 def dataset_treatment(data: pd.DataFrame) -> pd.DataFrame:
 
-    # -- Imóveis do tipo Hoteis são muito peculiares --------
-    data = data.loc[data["type"] != "HOTEL"].copy()
+    data = treat_type(data)
 
-    # -- Terrenos não tem certas propriedades ----------
-    data.loc[
-        data["type"].str.contains("ALLOTMENT"),
-        ["n_parking_spaces", "n_bathrooms", "n_bedrooms"],
-    ] = np.nan
+    data = treat_latlong(data)
 
-    # -- Unificando tipos comuns -----
-    data["type"] = data["type"].replace(TYPE_MAPPING)
-
-    # -- Gerando variáveis dummy para is tipos ------
-    categories = ["APARTMENT", "HOME", "ALLOTMENT_LAND", "COUNTRY"]
-    data = generate_dummy_variables(data, "type", categories, prefix="column")
-
-    # -- Anulando os casos onde a área é zero ------------
-    data.loc[
-        (data["type"].isin(["ALLOTMENT_LAND", "COUNTRY", "BUSINESS"]))
-        & (data["area"] == 0),
-        ["area"],
-    ] = np.nan
+    data = treat_neighborhood(data)
 
     return data
 
@@ -120,3 +113,88 @@ def generate_dummy_variables(
         )
 
     return dataframe
+
+
+def construct_features(data: pd.DataFrame) -> pd.DataFrame:
+
+    # data["has_condo_fee"] = np.where(data["condo_fee"] > 0, 1, 0)
+
+    return data
+
+
+def treat_type(data: pd.DataFrame) -> pd.DataFrame:
+
+    # -- Imóveis do tipo Hoteis são muito peculiares --------
+    data = data.loc[data["type"] != "HOTEL"].copy()
+
+    # -- Terrenos não tem certas propriedades ----------
+    data.loc[
+        data["type"].str.contains("ALLOTMENT"),
+        ["n_parking_spaces", "n_bathrooms", "n_bedrooms"],
+    ] = np.nan
+
+    # -- Unificando tipos comuns -----
+    data["type"] = data["type"].replace(TYPE_MAPPING)
+
+    # -- Gerando variáveis dummy para os tipos ------
+    categories = ["APARTMENT", "HOME", "ALLOTMENT_LAND", "COUNTRY"]
+    data = generate_dummy_variables(data, "type", categories, prefix="column")
+
+    # -- Anulando os casos onde a área é zero ------------
+    data.loc[
+        (data["type"].isin(["ALLOTMENT_LAND", "COUNTRY", "BUSINESS"]))
+        & (data["area"] == 0),
+        ["area"],
+    ] = np.nan
+
+    return data
+
+
+def treat_latlong(data: pd.DataFrame) -> pd.DataFrame:
+
+    # -- Removendo as latitudes e longitudes zeradas ---------
+    data.loc[data["longitude"] > -10, "longitude"] = np.nan
+    data.loc[data["latitude"] > -10, "latitude"] = np.nan
+
+    # -- tratando os casos onde latitude e longitude estão invertidas
+    conditions_lat_long_inverted = data["latitude"].between(*LONG_INTERVAL) & data[
+        "longitude"
+    ].between(*LAT_INTERVAL)
+
+    values = data.loc[conditions_lat_long_inverted][["latitude", "longitude"]].to_dict(
+        orient="records"
+    )[0]
+
+    data.loc[conditions_lat_long_inverted, "latitude"] = values["longitude"]
+    data.loc[conditions_lat_long_inverted, "longitude"] = values["latitude"]
+
+    # -- Anulando as latitudes e longitudes fora do intervalo -----
+    data.loc[~data["latitude"].between(*LAT_INTERVAL), "latitude"] = np.nan
+    data.loc[~data["longitude"].between(*LONG_INTERVAL), "longitude"] = np.nan
+
+    return data
+
+
+def treat_neighborhood(data: pd.DataFrame) -> pd.DataFrame:
+    def decode(x):
+        if x is not None:
+            return unidecode.unidecode(x)
+        else:
+            return x
+
+    try:
+        maps = get_config(filename="config/neighbor_rename.yaml")
+    except:
+        maps = {}
+
+    data["neighborhood"] = data["neighborhood"].replace(maps)
+
+    data["neighborhood"] = data["neighborhood"].apply(decode)
+
+    data["neighborhood"] = data["neighborhood"].apply(
+        lambda x: to_snake_case(x) if x is not None else None
+    )
+
+    data.loc[data["neighborhood"] == "", "neighborhood"] = None
+
+    return data
